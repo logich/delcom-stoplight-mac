c432daef63f98ec0cab6d20c22a9f0db462f680b
Logan Browne <logan@rosetrace.com>
2012-04-30 14:25:02 -0700

    modified perl script example to use File:Tail

--- delcom-stoplight/Lync-Availability-Monitor.pl
+++ delcom-stoplight/Lync-Availability-Monitor.pl
@@ -14,20 +14,6 @@
 # example working line is 
 #     perl -e 'open($h,$ARGV[0]); while (1) { /IPaddress/ and print "LINE: $_" for <$h>; sleep 1 }' snmplistener.log
 
-
-
-while (1) { 
-    $line = <>;
-    if($line =~ /PublishOrRemoveState: \"availability - 3500/) 
-    {
-        print ("LINE: $_\n");
-        system("/usr/local/bin/delcom-stoplight", "green");
-		sleep 1;
-    }
-    
-    /PublishOrRemoveState: \"availability -/ and print ("LINE: $_\n");
-}
-
 # line looks like: 	04/26/2012|16:16:14.998 143F:AC5642C0 INFO  :: PublishOrRemoveState: "availability - 6500, starttime - 0.000000, uri - , manual state - 1" "custom id - 0, activity count - 0"
 # values are:
 # 3500 - Available
@@ -36,3 +22,30 @@ while (1) {
 # 12500 - BRB
 # 15500 - Off Work or Away
 # 
+
+use File::Tail;
+
+$file=File::Tail->new("$ENV{HOME}/Library/Logs/Microsoft-Lync-0.log");
+
+while (defined($line=$file->read)) {
+#while (1) { 
+#    $line = <>;
+    if($line =~ /PublishOrRemoveState/) 
+    {
+        print ("LINE: $line\n");
+        if ($line =~ /availability - 3500/){
+        system("/usr/local/bin/delcom-stoplight", "green");
+        }
+        if ($line =~ /availability - 6500/){
+            system("/usr/local/bin/delcom-stoplight", "red");
+        }
+        if ($line =~ /availability - 12500/){
+            system("/usr/local/bin/delcom-stoplight", "yellow");
+        }
+		sleep 1;
+    }
+    
+    
+    /PublishOrRemoveState: \"availability - / and print ("LINE: $_\n");
+}
+
